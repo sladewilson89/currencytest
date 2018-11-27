eeeebfb4be946028443a0e1aa56cfd3ca5b05993
sladewilson89 <45387271+sladewilson89@users.noreply.github.com>
2018-11-27 15:41:20 +0530

    dd

--- CurrencyRecognition/currency_IR/run.sh
+++ CurrencyRecognition/currency_IR/run.sh
@@ -7,12 +7,12 @@ fi
 topKValue=10;
 
 
-for f in $2/ten/*.jpg; do echo $f;done > train_files/ten.txt
-for f in $2/twenty/*.jpg; do echo $f;done > train_files/twenty.txt
-for f in $2/fifty/*.jpg; do echo $f;done > train_files/fifty.txt
-for f in $2/hundred/*.jpg; do echo $f;done > train_files/hundred.txt
-for f in $2/fivehundred/*.jpg; do echo $f;done > train_files/fivehundred.txt
-for f in $2/thousand/*.jpg; do echo $f;done > train_files/thousand.txt
+for f in $2/ten/*.JPG; do echo $f;done > train_files/ten.txt
+for f in $2/twenty/*.JPG; do echo $f;done > train_files/twenty.txt
+for f in $2/fifty/*.JPG; do echo $f;done > train_files/fifty.txt
+for f in $2/hundred/*.JPG; do echo $f;done > train_files/hundred.txt
+for f in $2/fivehundred/*.JPG; do echo $f;done > train_files/fivehundred.txt
+for f in $2/thousand/*.JPG; do echo $f;done > train_files/thousand.txt
 
 g++ `pkg-config --cflags --libs opencv` testCurrency.cpp -o currency-test -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_objdetect -lopencv_flann -lopencv_calib3d -lopencv_xfeatures2d -lopencv_features2d -lopencv_imgcodecs;
 ./currency-test $1/dataFile.txt $1/vocabulary.bin $1/labels.txt $1/indicesSize.txt $1/allIndex.bin $topKValue $1/keypoints/ train_files/
